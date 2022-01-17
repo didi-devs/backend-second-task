71881a48f198240b5505897e61330a507672b3ff
Divine-Favour Oladejo <88663591+didi-devs@users.noreply.github.com>
2022-01-17 12:48:34 +0100

    Backend Series

--- myApp.js
+++ myApp.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var app = express();
 
+console.log("Hello World")
+
+app.get("/", (req, res) => {
+    res.send('Hello Express');
+  });
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/views/index.html");
+  });
+
+app.use("/public", express.static())
 
 
-
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/views/style.css");
+  });
 
 
 
