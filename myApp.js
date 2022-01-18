38bec6215c85845870d1bd10330a091014f7bd8b
Divine-Favour Oladejo <88663591+didi-devs@users.noreply.github.com>
2022-01-18 16:45:50 +0100

    make it better

--- myApp.js
+++ myApp.js
@@ -4,21 +4,10 @@ var app = express();
 
 console.log("Hello World")
 
-app.get("/", (req, res) => {
-    res.send('Hello Express');
-  });
 
 app.get("/", function(req, res) {
-    res.sendFile(__dirname + "/views/index.html");
-  });
-
-app.use("/public", express.static())
-
-
-app.get("/", function(req, res) {
-    res.sendFile(__dirname + "/views/style.css");
-  });
-
+  res.send("Hello Express");
+});
 
 
 
